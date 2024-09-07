e25aa8c25621a06484db8314c9ca872b69e323eb
Dan Couture <mathyourlife@gmail.com>
2024-09-06 22:52:46 -0400

    constants and formatting

--- Rainbow/Rainbow.ino
+++ Rainbow/Rainbow.ino
@@ -3,24 +3,26 @@
 
 // Specify which digital IO port the data wire from the LED strip
 // will be connected to.
-#define LED_PIN  7
+#define LED_PIN     7
 
 // Specify how many LEDs on the strip will be controlled.
-#define NUM_LEDS 144
+#define NUM_LEDS    144
+
+// Specify the color order of the LEDs. It differs by type of LED strip
+// and could be RGB, RBG, BRG, GBR, ...
+// Use ThreeLightTest to determine the correct order.
+#define COLOR_ORDER GRB
+
+// LED chipset type.
+#define CHIPSET     WS2812B
 
 // Create a variable that will store the colors for the LEDs.
 CRGB leds[NUM_LEDS];
 uint8_t hue = 0;
 
 void setup() {
-    // Tell the FastLED library:
-    //   * WS2812B - which type of LEDs will be controlled
-    //   * LED_PIN - which pin to send out the data signal
-    //   * RGB - the color order. It differs by type of LED strip
-    //           and could be RGB, RBG, BRG, GBR, ...
-    //   * leds - the variable that is storing all the colors
-    //   * NUM_LEDS - the number of LEDs to control
-    FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+    // Configure the FastLED library to the LED strip.
+    FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
 
     // clear all pixel data
     FastLED.clear(true);
